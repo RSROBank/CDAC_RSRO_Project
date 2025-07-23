278c7c76852184ab8bd45dd385eeb8445b90bade
Sudhir Singh <sudhirsingh72065@gmail.com>
2025-07-24 00:49:52 +0530

    Dashboard part modified

---
+++ Frontend/category-products-frontend/src/components/EmployeeDashBoard/Sidebar.jsx
@@ -0,0 +1,33 @@
+// src/components/Dashboard/Sidebar.js
+import React from 'react';
+
+const options = [
+  'Dashboard',
+  'Statements',
+  'Send Money',
+  'Apply for Loan/FD',
+  'Loan',
+  'Manage Cards',
+  'Deposit'
+];
+
+const Sidebar = ({ selected, setSelected }) => {
+  return (
+    <div className="w-1/4 bg-white border-r p-4 space-y-2">
+      {options.map(option => (
+        <button
+          key={option}
+          className={`w-full py-2 px-3 text-left border rounded mt-4
+             hover:bg-blue-100 ${
+            selected === option ? 'bg-blue-200' : ''
+          }`}
+          onClick={() => setSelected(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
