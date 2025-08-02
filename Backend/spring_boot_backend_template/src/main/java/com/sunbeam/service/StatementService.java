0e01c7158482db06d43f16a1b3e0ea07435bb05b
rsravi-singh <shraviranjan7@gmail.com>
2025-08-02 20:24:55 +0530

    statement modified

--- Backend/spring_boot_backend_template/src/main/java/com/sunbeam/service/StatementService.java
+++ Backend/spring_boot_backend_template/src/main/java/com/sunbeam/service/StatementService.java
@@ -2,9 +2,14 @@ package com.sunbeam.service;
 
 import java.time.LocalDate;
 
+import com.sunbeam.dto.StatementRequestDTO;
 import com.sunbeam.dto.StatementResponseDTO;
 
 public interface StatementService {
 
-	public StatementResponseDTO getStatement(String accountNo, LocalDate fromDate, LocalDate toDate, String type);
+	
+
+	public StatementResponseDTO getStatement(StatementRequestDTO requestDTO);
+
 }
+ 
