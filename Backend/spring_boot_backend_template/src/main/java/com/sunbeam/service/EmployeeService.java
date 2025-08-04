9148fef14ab2b5e1b714a602b1a5a1942135af57
KD2-89213-OMKAR <omkartelang0599@gmail.com>
2025-08-04 15:40:51 +0530

    employee services and controller is created

--- Backend/spring_boot_backend_template/src/main/java/com/sunbeam/service/EmployeeService.java
+++ Backend/spring_boot_backend_template/src/main/java/com/sunbeam/service/EmployeeService.java
@@ -1,5 +1,14 @@
 package com.sunbeam.service;
 
-public interface EmployeeService {
+import java.util.List;
 
+import com.sunbeam.dto.ApiResponse;
+import com.sunbeam.dto.EmployeeResponseDTO;
+
+public interface EmployeeService {
+    EmployeeResponseDTO createEmployee(EmployeeResponseDTO employeeResponseDTO);
+    EmployeeResponseDTO getEmployeeById(Long id);
+    List<EmployeeResponseDTO> getAllEmployees();
+    EmployeeResponseDTO updateEmployee(Long id, EmployeeResponseDTO employeeDTO);
+    ApiResponse deleteEmployee(Long id);
 }
